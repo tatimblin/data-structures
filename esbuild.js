55f9daada2d7cfbe5b0cb0cf6c9583cdeb6e0679
Tristan Timblin <tatimblin@gmail.com>
2023-07-14 23:16:01 -0400

    generate type declarations

--- esbuild.js
+++ esbuild.js
@@ -1,13 +1,7 @@
-const { Generator } = require("npm-dts");
 const { build } = require("esbuild");
 
-new Generator({
-    entry: "src/index.ts",
-    output: "dist/index.d.ts",
-}).generate();
-
 build({
-    entryPoints: ["src/index.ts"],
+    entryPoints: ["./src/index.ts"],
     outdir: "dist",
     bundle: true,
     minify: true,
