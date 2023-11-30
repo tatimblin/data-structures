e91d4c7d4916256fdd5603354cd3c0c1804405d5
Tristan Timblin <tatimblin@gmail.com>
2023-11-29 22:04:30 -0800

    scaffold LRU

--- src/least-recently-used/LeastRecentlyUsed.test.ts
+++ src/least-recently-used/LeastRecentlyUsed.test.ts
@@ -1,7 +1,51 @@
-import { LinkedList } from "../index.js";
+import { LeastRecentlyUsed } from "./index.js";
 
 describe("LeastRecentlyUsed", () => {
-    it("", () => {
+    describe("push()", () => {
+        it("", () => {
+            const LRU = new LeastRecentlyUsed();
+        });
+    });
 
+    describe("shift()", () => {
+        it("", () => {
+            const LRU = new LeastRecentlyUsed();
+        });
+    });
+
+    describe("pop()", () => {
+        it("", () => {
+            const LRU = new LeastRecentlyUsed();
+        });
+    });
+
+    describe("get()", () => {
+        it("", () => {
+            const LRU = new LeastRecentlyUsed();
+        });
+    });
+
+    describe("has()", () => {
+        it("", () => {
+            const LRU = new LeastRecentlyUsed();
+        });
+    });
+
+    describe("list()", () => {
+        it("", () => {
+            const LRU = new LeastRecentlyUsed();
+        });
+    });
+
+    describe("delete()", () => {
+        it("", () => {
+            const LRU = new LeastRecentlyUsed();
+        });
+    });
+
+    describe("clear()", () => {
+        it("", () => {
+            const LRU = new LeastRecentlyUsed();
+        });
     });
 });
--- src/least-recently-used/LeastRecentlyUsed.ts
+++ src/least-recently-used/LeastRecentlyUsed.ts
@@ -2,4 +2,32 @@ export class LeastRecentlyUsed {
     constructor() {
 
     }
+
+    push() {
+        return new Error("Not yet implemented");
+    }
+
+    shift() {
+        return new Error("Not yet implemented");
+    }
+
+    pop() {
+        return new Error("Not yet implemented");
+    }
+
+    get() {
+        return new Error("Not yet implemented");
+    }
+
+    list() {
+        return new Error("Not yet implemented");
+    }
+
+    delete() {
+        return new Error("Not yet implemented");
+    }
+
+    clear() {
+        return new Error("Not yet implemented");
+    }
 }
